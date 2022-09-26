148532338a88f35055d5d74afcf40c687e759c3e
tanveerprottoy <tanveer.prottoy@gmail.com>
2022-09-26 14:56:14 +0600

    add db setup

---
+++ pkg/data/sql/postgres/db.go
@@ -0,0 +1,44 @@
+package postgres
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "root"
+	dbname   = "gateway_service"
+)
+
+var (
+	DB  *sql.DB
+	err error
+)
+
+func init() {
+	args := fmt.Sprintf(
+		"host=%s port=%d user=%s "+
+			"password=%s dbname=%s sslmode=disable",
+		host,
+		port,
+		user,
+		password,
+		dbname,
+	)
+	DB, err = sql.Open("postgres", args)
+	if err != nil {
+		panic(err)
+	}
+	// ping is necessary to create connection
+	err = DB.Ping()
+	if err != nil {
+		panic(err)
+	}
+	log.Println("Successfully connected!")
+}
