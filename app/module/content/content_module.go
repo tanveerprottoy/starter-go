29c7b7420c33c988e307368fac692d534f482e2a
tanveerprottoy <tanveer.prottoy@gmail.com>
2022-11-05 04:22:31 +0600

    add init order comment

--- app/module/content/content_module.go
+++ app/module/content/content_module.go
@@ -15,6 +15,8 @@ type ContentModule struct {
 
 func NewContentModule(db *mongo.Database, router *router.Router) *ContentModule {
 	m := new(ContentModule)
+	// init order is reversed of the field decleration
+	// as the dependency is served this way
 	m.ContentRepository = new(ContentRepository)
 	m.ContentService = NewContentService(m.ContentRepository)
 	m.ContentHandler = NewContentHandler(m.ContentService)
