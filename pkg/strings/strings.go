5971898813704e1946ab95cda68e99afc6004256
tanveerprottoy <tanveer.prottoy@gmail.com>
2022-12-08 15:42:24 +0600

    add string functions

--- pkg/strings/strings.go
+++ pkg/strings/strings.go
@@ -5,3 +5,15 @@ import "strings"
 func Split(v, sep string) []string {
 	return strings.Split(v, " ")
 }
+
+func Trim(v, cutset string) string {
+	return strings.Trim(v, cutset)
+}
+
+func ToLower(v string) string {
+	return strings.ToLower(v)
+}
+
+func ToUpper(v string) string {
+	return strings.ToUpper(v)
+}
