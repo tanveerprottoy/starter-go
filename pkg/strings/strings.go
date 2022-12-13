02b199ae97c6d8e546b73dd263273e8be070c1a5
tanveerprottoy <tanveer.prottoy@gmail.com>
2022-12-13 18:28:14 +0600

    add strings functions

--- pkg/strings/strings.go
+++ pkg/strings/strings.go
@@ -10,6 +10,14 @@ func Trim(v, cutset string) string {
 	return strings.Trim(v, cutset)
 }
 
+func TrimSpace(v string) string {
+	return strings.TrimSpace(v)
+}
+
+func TrimSuffix(v, suffix string) string {
+	return strings.TrimSuffix(v, suffix)
+}
+
 func ToLower(v string) string {
 	return strings.ToLower(v)
 }
