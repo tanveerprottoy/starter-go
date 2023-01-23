e56902ebaa04e218184dcc36b0d2aebb2b7fb3d5
tanveerprottoy <tanveer.prottoy@gmail.com>
2023-01-23 12:45:23 +0600

    add new code

--- net-http/pkg/json/json.go
+++ net-http/pkg/json/json.go
@@ -13,6 +13,10 @@ func Unmarshal(d []byte, v any) error {
 	return json.Unmarshal(d, &v)
 }
 
+func Encode(w io.Writer, v any) error {
+	return json.NewEncoder(w).Encode(&v)
+}
+
 func Decode(r io.Reader, v any) error {
 	return json.NewDecoder(r).Decode(&v)
 }
