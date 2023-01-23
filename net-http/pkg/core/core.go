684030376df3518474e80f12e5e7eb745a35bb2f
tanveerprottoy <tanveer.prottoy@gmail.com>
2023-01-24 00:28:51 +0600

    add refactors6

--- net-http/pkg/core/core.go
+++ net-http/pkg/core/core.go
@@ -24,6 +24,24 @@ func ExtractFieldsFromStruct[T any](t *T) []any {
 	return f
 }
 
+func CheckTypeInt(x any) bool {
+	switch x.(type) {
+	case int, int32, int64 :
+		return true
+	default:
+		return false
+	}
+}
+
+func CheckTypeFloat(x any) bool {
+	switch x.(type) {
+	case float32, float64 :
+		return true
+	default:
+		return false
+	}
+}
+
 func TypeCastIfInt(x any) (int32, error) {
 	switch v := x.(type) {
 	case int, int32, int64 :
