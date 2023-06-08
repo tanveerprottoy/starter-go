64ca1411ec7ca66710bc3e4b93ae67017223f917
tanveerprottoy <tanveer.prottoy@gmail.com>
2023-06-08 17:02:55 +0600

    add redis client

--- stdlib/pkg/redispkg/client.go
+++ stdlib/pkg/redispkg/client.go
@@ -1,20 +1,24 @@
 package redispkg
 
-import "github.com/redis/go-redis/v9"
+import (
+	"context"
+	"github.com/redis/go-redis/v9"
+	"github.com/tanveerprottoy/starter-go/pkg/config"
+)
 
 type Client struct {
-
+	client *redis.Client
 }
 
 func (c *Client) init() {
-	rdb := redis.NewClient(&redis.Options{
-        Addr:     "localhost:6379",
-        Password: "", // no password set
-        DB:       0,  // use default DB
-    })
-
-    err := rdb.Set(ctx, "key", "value", 0).Err()
-    if err != nil {
-        panic(err)
-    }
+	c.client = redis.NewClient(&redis.Options{
+		Addr:     config.GetEnvValue("REDIS_URL"),
+		Password: config.GetEnvValue("REDIS_PASS"), // "" empty for no password set
+		DB:       0,                                // use default DB
+	})
+	ctx := context.Background()
+	err := rdb.Set(ctx, "key", "value", 0).Err()
+	if err != nil {
+		panic(err)
+	}
 }
