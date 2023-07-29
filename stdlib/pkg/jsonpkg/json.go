9d72f30e47ded46789c6ac18a157b10f84001333
tanveerprottoy <tanveer.prottoy@gmail.com>
2023-07-29 18:26:57 +0600

    add json.go refactor

--- stdlib/pkg/jsonpkg/json.go
+++ stdlib/pkg/jsonpkg/json.go
@@ -17,12 +17,12 @@ func Unmarshal(d []byte, v any) error {
 }
 
 // Encode Encode writes the JSON encoding of v to the stream which is provided by the encoder created from the passed io.writer
-func Encode(v any, w io.Writer) error {
+func Encode(w io.Writer, v any) error {
 	return json.NewEncoder(w).Encode(v)
 }
 
 // Decode reads the JSON value from decoder created from the passed io.reader
 // The caller must pass the address for the v any param, ex: &v
-func Decode(v any, r io.Reader) error {
+func Decode(r io.Reader, v any) error {
 	return json.NewDecoder(r).Decode(v)
 }
